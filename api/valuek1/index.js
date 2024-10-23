084ae51f822670caba92984448e268a69387186c
shanefieldj <shanefieldjacob@gmail.com>
2024-10-23 18:54:03 -0400

    new dev setup

--- api/valuek1/index.js
+++ api/valuek1/index.js
@@ -1,5 +1,8 @@
 module.exports = async function (context, req) {
+    const apiKey = process.env.kvalue;
+
     context.res.json({
-        text: "Hello from the API"
+        text: "Hello from the new API",
+        apiKey: apiKey // You can include this in the response, or use it elsewhere
     });
 };
